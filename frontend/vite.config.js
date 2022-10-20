6419e7a0f5e18fa9b7f82dbdffc5312a6ecdfaf5
Shad <shadrais321@outlook.com>
2022-10-21 00:16:40 +0530

    Fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   external: ['Buffer'],
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
     },
   },
 })
